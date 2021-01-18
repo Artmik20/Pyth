1091c646b22a5eb3dae3ac720eb51eee480cc318
artmik20 <artmik20@gmail.com>
2021-01-18 19:43:26 +0300

    [upd]

--- 1.py
+++ 1.py
@@ -1,12 +1,11 @@
-my_f = open('file1.txt', 'w')
+my_f = open('../Урок5 дз/file1.txt', 'w')
 
 with my_f:
     while True:
         b = (input("Введите текст "))
         my_f.write(f"{b}\n")
 
-        if not b:
-            break
+
 
 
 
